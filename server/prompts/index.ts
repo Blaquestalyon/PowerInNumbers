e064e7a6cc8e4cc98f1116d8bc29516660d47f36
PIN Build <build@power-in-numbers.net>
2026-05-05 05:55:16 -0500

    Revert "v8.6: standalone HTML report export with working Monte Carlo"
    This reverts commit c6a22cb02d864334622524dc9c37115baa198791.

--- server/prompts/index.ts
+++
@@ -1,5 +0,0 @@
-import { CORE_A1A_PROMPT } from './core-a1a';
-import { CORE_A1B_PROMPT } from './core-a1b';
-import { SECTION_PROMPTS } from './sections';
-
-export { CORE_A1A_PROMPT, CORE_A1B_PROMPT, SECTION_PROMPTS };
